a1c81092560b0eb6f045c4f8c3b734c8eb0be428
dhruvgoyal-star <dhruvthegreatestgoyal@gmail.com>
2025-11-25 15:01:10 +0530

    Implement Bottom Navigation Bar with screens

---
+++ lib/widgets/bottom_nav.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:sanwariyaseth_pvt/screens/home_screen.dart';
+import 'package:sanwariyaseth_pvt/screens/news_screen.dart';
+import 'package:sanwariyaseth_pvt/screens/chat_screen.dart';
+import 'package:sanwariyaseth_pvt/screens/profile_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    NewsScreen(),
+    ChatScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.newspaper), label: "News"),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
